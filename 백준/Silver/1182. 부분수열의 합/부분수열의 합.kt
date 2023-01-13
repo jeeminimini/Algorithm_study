7a46aeffd944f63d57af3933244c3dd6dd45a9b4
이지민 <74500793+jeeminimini@users.noreply.github.com>
2023-01-13 20:31:31 +0900

    [Silver II] Title: 부분수열의 합, Time: 180 ms, Memory: 21432 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.kt
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.kt
@@ -1,7 +1,5 @@
 package jimin.`18week`
 
-import kotlin.math.abs
-
 var n = 0
 var s = 0
 var result = 0
@@ -12,33 +10,20 @@ fun main() {
     }
     val numbers = readln().split(" ").map { it.toInt() } as MutableList
     numbers.sort()
-    getNumber(numbers, MutableList(n) { false }, 0)
+    getNumber(numbers, 0, 0)
     println(result)
 }
 
-fun getNumber(numbers: MutableList<Int>, visited: MutableList<Boolean>, idx: Int) {
-    var sum = 0
-    if (visited.any { it }) {
-        visited.forEachIndexed { idx, visit ->
-            if (visit) {
-                sum += numbers[idx]
-            }
-        }
-        //println("visited $visited sum $sum")
+fun getNumber(numbers: MutableList<Int>, idx: Int, sum: Int) {
+    if (idx != 0) {
         if (sum * s <= 0 && sum > s) {
-            //println("탈출!")
             return@getNumber
         } else if (sum == s) {
             result += 1
-            //println("정답")
         }
     }
 
     for (i in idx until n) {
-        if (visited[i].not()) {
-            visited[i] = true
-            getNumber(numbers, visited, i + 1)
-            visited[i] = false
-        }
+        getNumber(numbers, i + 1, sum + numbers[i])
     }
 }
