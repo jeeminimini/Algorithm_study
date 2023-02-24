e02344b937a1c4356f8a2debef8a4c7be150cf3e
이지민 <74500793+jeeminimini@users.noreply.github.com>
2023-02-25 02:15:40 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 13.78 ms, Memory: 62.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.kt
@@ -0,0 +1,32 @@
+import kotlin.math.*
+
+class Solution {
+    lateinit var treeInfo: MutableList<Int>
+    lateinit var visited: MutableList<Boolean>
+    fun solution(n: Int, wires: Array<IntArray>): Int {
+        var answer: Int = Int.MAX_VALUE
+        var tree = MutableList(n + 1){ mutableListOf<Int>() }
+        treeInfo = MutableList(n + 1){ 1 }
+        visited = MutableList(n + 1){ false }
+        wires.forEach{ w ->
+            tree[w.first()].add(w.last())
+            tree[w.last()].add(w.first())
+        }
+        visited[1] = true
+        dfs(1, tree)
+        treeInfo.forEach{
+            answer = min(answer, abs(n - it - it))
+        }
+        return answer
+    }
+    
+    fun dfs(root: Int, tree: MutableList<MutableList<Int>>) {
+        tree[root].forEach{ t ->
+            if (visited[t].not()){
+                visited[t] = true
+                dfs(t, tree)
+                treeInfo[root] += treeInfo[t]
+            }
+        }
+    }
+}
