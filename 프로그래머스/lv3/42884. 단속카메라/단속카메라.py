9a009eb66589530087ae20f4ba49b7de184596f1
이지민 <74500793+jeeminimini@users.noreply.github.com>
2023-08-30 13:30:28 +0900

    [level 3] Title: 단속카메라, Time: 2.51 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,28 @@
+def solution(routes):
+    maxi = max(map(max, routes))
+    mini = min(map(min, routes))
+    
+    routes = sorted(routes, key = lambda x: x[1])
+    
+    idx_s = 0
+    idx_n = 1
+    total = 0
+    
+    while True:
+        # print(f"idx_s {idx_s} idx_n {idx_n}")
+        if idx_n == len(routes):
+            break
+        if routes[idx_s][1] < routes[idx_n][0]:
+            total += 1
+            idx_s = idx_n
+        idx_n += 1
+    
+    return total + 1
+
+# print(solution([[-2,-1], [1,2],[-3,0]])) #2
+# print(solution([[0,0],])) #1
+# print(solution([[0,1], [0,1], [1,2]])) #1
+# print(solution([[0,1], [2,3], [4,5], [6,7]])) #4
+# print(solution([[-20,-15], [-14,-5], [-18,-13], [-5,-3]])) #2
+# print(solution([[-20,15], [-14,-5], [-18,-13], [-5,-3]])) #2
+# print(solution([[-20,15], [-20,-15], [-14,-5], [-18,-13], [-5,-3]])) #2
