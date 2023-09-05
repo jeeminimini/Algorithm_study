f56253b3dd0edabe32d6c42c592c8ec06f5cac05
이지민 <74500793+jeeminimini@users.noreply.github.com>
2023-09-05 14:06:39 +0900

    [level 3] Title: 가장 먼 노드, Time: 7564.58 ms, Memory: 20.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def solution(n, edge):
+    graphs = dict()
+    for a, b in edge:
+        if a in graphs.keys():
+            graphs[a].append(b)
+        else:
+            graphs[a] = [b]
+            
+        if b in graphs.keys():
+            graphs[b].append(a)
+        else:
+            graphs[b] = [a]
+    
+    queue = deque([1])
+    visited = [0 for _ in range(n + 1)]
+    visited[1] = 1
+    num = 1
+    while queue:
+        now = queue.popleft()
+        for m in graphs[now]:
+            if visited[m] == 0:
+                visited[m] = visited[now] + 1
+                queue.append(m)
+        
+    result = sum(map(lambda x: 1 if x == max(visited) else 0, visited))
+    return result
