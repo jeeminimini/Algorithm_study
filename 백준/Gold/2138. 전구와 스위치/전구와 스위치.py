ecc1dd1fb6785c7a9b567183db05fc84d600f23c
이지민 <74500793+jeeminimini@users.noreply.github.com>
2023-09-11 02:48:09 +0900

    [Gold V] Title: 전구와 스위치, Time: 132 ms, Memory: 34380 KB -BaekjoonHub

---
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
@@ -0,0 +1,35 @@
+n = int(input())
+before = list(map(int, list(input())))
+after = list(map(int, list(input())))
+
+# 첫번째 전구 안누름
+now_1 = before[:]
+num_1 = 0
+for i in range(0, n - 1):
+    if now_1[i] != after[i]:
+        num_1 += 1
+        now_1[i] = 1 - now_1[i]
+        now_1[i + 1] = 1 - now_1[i + 1]
+        if i + 2 < n:
+            now_1[i + 2] = 1 - now_1[i + 2]
+
+now_2 = before[:]
+now_2[0] = not now_2[0]
+now_2[1] = not now_2[1]
+num_2 = 1
+for i in range(0, n - 1):
+    if now_2[i] != after[i]:
+        num_2 += 1
+        now_2[i] = 1 - now_2[i]
+        now_2[i + 1] = 1 - now_2[i + 1]
+        if i + 2 < n:
+            now_2[i + 2] = 1 - now_2[i + 2]
+
+if now_1 != after and now_2 == after:
+    print(num_2)
+elif now_2 != after and now_1 == after:
+    print(num_1)
+elif now_1 == after and now_2 == after:
+    print(min(num_1, num_2))
+else:
+    print(-1)
