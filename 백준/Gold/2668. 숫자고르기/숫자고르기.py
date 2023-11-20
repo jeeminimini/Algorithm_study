d8e029294f5521d9f97aef8e78107a22e2c91e18
이지민 <74500793+jeeminimini@users.noreply.github.com>
2023-11-20 18:46:21 +0900

    [Gold V] Title: 숫자고르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.py
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -1,3 +1,11 @@
+'''
+처음에는 그냥 dfs로 했더니 시간초과났다
+
+사이클을 도는 친구를 찾아야한다..
+https://velog.io/@deannn/BOJ-%EB%B0%B1%EC%A4%80-2668%EB%B2%88-%EC%88%AB%EC%9E%90%EA%B3%A0%EB%A5%B4%EA%B8%B0-Python
+참고
+'''
+
 import sys
 n = int(sys.stdin.readline())
 numbers = [0]
@@ -13,8 +21,8 @@ def dfs(first, second, now):
     if numbers[now] in first:
         if first == second:
             answer.update(first)
-            return True
-        return False
+            return
+        return
     return dfs(first, second, numbers[now])
 
 
