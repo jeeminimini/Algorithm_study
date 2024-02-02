3b20f96f01e06e1b0e1a43a831400d9d423a0b51
이지민 <74500793+jeeminimini@users.noreply.github.com>
2024-02-02 11:52:55 +0900

    [Silver V] Title: 집합, Time: 1288 ms, Memory: 304164 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.kt
+++ 백준/Silver/11723. 집합/집합.kt
@@ -1,42 +1,57 @@
-package jimin.`31week`
+package jimin
 
-class 집합 {
-    fun solve() {
-        var num = 0
-        val sb = StringBuilder()
-        repeat(readln().toInt()) {
-            val operators = readln().split(" ")
-            when (operators.first()) {
-                "add" -> {
-                    num = num or (1 shl operators.last().toInt() - 1)
-                }
-
-                "remove" -> {
-                    num = num and (1 shl operators.last().toInt() - 1).inv()
-                }
-
-                "check" -> {
-                    val tmp = num and (1 shl operators.last().toInt() - 1)
-                    if (tmp == 0) sb.append("0\n") else sb.append("1\n")
-                }
-
-                "toggle" -> {
-                    num = num xor (1 shl operators.last().toInt() - 1)
-                }
-
-                "all" -> {
-                    num = (1 shl 20).inv()
-                }
-
-                "empty" -> {
-                    num = 0
-                }
-            }
-        }
-        println(sb.toString())
-    }
-}
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
 fun main() {
-    집합().solve()
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val m = br.readLine().toInt()
+    var s = MutableList(21) { false }
+
+    var operator = ""
+    var x = 0
+
+    repeat(m) {
+        br.readLine().split(" ").apply {
+            operator = first()
+            x = last().toIntOrNull() ?: 0
+        }
+
+        when (operator) {
+            "add" -> {
+                s[x] = true
+            }
+
+            "remove" -> {
+                s[x] = false
+            }
+
+            "check" -> {
+                if (s[x]) {
+                    bw.write("1\n")
+                } else {
+                    bw.write("0\n")
+                }
+            }
+
+            "toggle" -> {
+                s[x] = s[x].not()
+            }
+
+            "all" -> {
+                s = MutableList(21) { true }
+            }
+
+            "empty" -> {
+                s = MutableList(21) { false }
+            }
+        }
+    }
+
+    bw.flush()
+    bw.close()
 }
