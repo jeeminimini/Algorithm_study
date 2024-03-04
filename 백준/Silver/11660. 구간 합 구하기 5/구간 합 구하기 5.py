0acdef28c2877886f3ebd9b5dba8a99456e29368
이지민 <74500793+jeeminimini@users.noreply.github.com>
2024-03-05 03:23:01 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 992 ms, Memory: 106608 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -2,29 +2,21 @@ import sys
 
 n, m = map(int, sys.stdin.readline().split())
 numbers = []
-sum_info = []
+sum_info = [[0 for _ in range(n + 1)]]
 
 for i in range(n):
     numbers.append(list(map(int, sys.stdin.readline().split())))
 
-for i in range(n):
-    sub_info = []
-    for j in range(n):
-        if j == 0:
-            sub_info.append(numbers[i][j])
-        else:
-            sub_info.append(sub_info[-1] + numbers[i][j])
+for i in range(1, n + 1):
+    sub_info = [0]
+    tmp = [0]
+    for j in range(1, n + 1):
+        tmp.append(tmp[-1] + numbers[i - 1][j - 1])
+        sub_info.append(sum_info[i - 1][j] + tmp[-1])
 
     sum_info.append(sub_info)
 
 for i in range(m):
-    x1, y1, x2, y2 = map(lambda x: int(x) - 1, sys.stdin.readline().split())
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
 
-    result = 0
-    for j in range(x1, x2 + 1):
-        if y1 == 0:
-            result += sum_info[j][y2]
-        else:
-            result += sum_info[j][y2] - sum_info[j][y1 - 1]
-
-    print(result)
+    print(sum_info[x2][y2] - sum_info[x1 - 1][y2] - sum_info[x2][y1 - 1] + sum_info[x1 - 1][y1 - 1])
