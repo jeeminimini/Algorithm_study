ee9ad421cb2682c68e9848624e69eb61a5a0a9c9
이지민 <74500793+jeeminimini@users.noreply.github.com>
2024-04-16 16:24:22 +0900

    [Gold V] Title: 동전, Time: 96 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -8,7 +8,7 @@ for i in range(t):
     answer_coin = int(sys.stdin.readline())
 
     dp = [0 for _ in range(answer_coin + 1)]
-
+    dp[0] = 1
     for i in range(len(coin_types)):
         for j in range(1, answer_coin + 1):
             if j < coin_types[i]:
@@ -17,9 +17,6 @@ for i in range(t):
                 if j % coin_types[i] == 0:
                     dp[j] = 1
             else:
-                if j == coin_types[i]:
-                    dp[j] = dp[j] + 1
-                else:
-                    dp[j] = dp[j] + dp[j - coin_types[i]]
+                dp[j] = dp[j] + dp[j - coin_types[i]]
 
     print(dp[-1])
